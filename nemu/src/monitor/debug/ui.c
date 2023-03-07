5e441d283129490c1f7e23602ed511a4cc2a91c3
tracer-ics2017 <tracer@njuics.org>
2023-03-07 19:11:42 +0000

    >  compile 2012109 ics Linux b87d0d6114df 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 GNU/Linux  19:11:42 up 2 days,  9:28,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,7 +36,8 @@ static int cmd_q(char *args) {
   return -1;
 }
 static int cmd_si(char *args) {
-  printf("si\n");
+  //printf("si\n");
+  cpu_exec(1);
   return -1;
 }
 static int cmd_help(char *args);
@@ -48,7 +49,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "excute next n structions and stop",cmd_si },
+  { "si", "excute next n structions and stop",cmd_si}
   /* TODO: Add more commands */
 
 };
