eb765b2414314712808037b87b6865da41e22483
tracer-ics2017 <tracer@njuics.org>
2023-03-09 06:42:44 +0000

    >  compile 2012109 ics Linux b87d0d6114df 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 GNU/Linux  06:42:44 up 3 days, 20:59,  0 users,  load average: 0.01, 0.04, 0.01 69b5157240c9644e561b279141d027a88b703ae5

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -112,18 +112,20 @@ static bool make_token(char *e) {
             tokens[nr_token].type=rules[i].token_type;
             for(int i=0;i<8;i++){
               if(strcmp(&e[position+1],regsl[i])==0){
-                tokens[nr_token++].val=reg_l(i);
+                tokens[nr_token].val=reg_l(i);
                 break;
               }
               if(strcmp(&e[position+1],regsb[i])==0){
-                tokens[nr_token++].val=reg_b(i);
+                tokens[nr_token].val=reg_b(i);
                 break;
               }
               if(strcmp(&e[position+1],regsw[i])==0){
-                tokens[nr_token++].val=reg_w(i);
+                tokens[nr_token].val=reg_w(i);
                 break;
               }
             }
+            nr_token++;
+            break;
           default:
             assert(0);
         }
