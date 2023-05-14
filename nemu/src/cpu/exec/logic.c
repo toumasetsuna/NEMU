fb77af30d9004b9a1a4a991426be7791134d55cd
tracer-ics2017 <tracer@njuics.org>
2023-05-14 17:00:41 +0000

    >  compile 2012109 ics Linux 444201d238c3 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 GNU/Linux  17:00:41 up 1 day,  5:17,  1 user,  load average: 0.03, 0.03, 0.05 c026b097adacd7382c2ae34fb723ce8f5c723b8b

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -50,18 +50,20 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  rtl_shl(&id_dest->val,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&id_dest->val);
-  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+ rtl_shl(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  print_asm_template2(shl);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  rtl_shr(&id_dest->val,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&id_dest->val);
-  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+  rtl_shr(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  print_asm_template2(shr);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
