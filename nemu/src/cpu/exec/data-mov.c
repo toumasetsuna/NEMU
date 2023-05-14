67f3ef16fac192f5a17b6fa4f799493de3a27c28
tracer-ics2017 <tracer@njuics.org>
2023-05-14 20:53:14 +0000

    >  compile 2012109 ics Linux 444201d238c3 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 GNU/Linux  20:53:14 up 1 day,  9:10,  1 user,  load average: 0.18, 0.54, 0.84 a8c2e7989ea78da2c929cb83d73fefc0b7e1f93b

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -57,12 +57,12 @@ make_EHelper(cltd) {
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
      rtl_lr_b(&t0, R_EAX);
-     rtl_sext(&t0, &t0, 2);
+     rtl_sext(&t0, &t0, 1);
      rtl_sr_w(R_EAX,&t0);
   }
   else {
     rtl_lr_w(&t0, R_EAX);
-     rtl_sext(&t0, &t0, 4);
+     rtl_sext(&t0, &t0, 2);
      rtl_sr_l(R_EAX,&t0);
   }
 
