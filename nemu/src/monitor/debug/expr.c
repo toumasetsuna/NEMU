3e11a30f69c9bbbc5e502efd33e2eb73b5cf60a5
tracer-ics2017 <tracer@njuics.org>
2023-05-14 15:43:32 +0000

    >  compile 2012109 ics Linux 444201d238c3 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 GNU/Linux  15:43:32 up 1 day,  4:00,  2 users,  load average: 0.07, 0.12, 0.19 9e8fa445656cdc96992a91783c1091822b169e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -76,6 +76,7 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        printf("1 token\n");
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
