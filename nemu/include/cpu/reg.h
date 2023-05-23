9d1554a15e1b298c8420894ecda37a57ca455084
tracer-ics2017 <tracer@njuics.org>
2023-05-23 16:10:42 +0000

    >  compile 2012109 ics Linux 444201d238c3 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 GNU/Linux  16:10:42 up 2 days, 19:22,  1 user,  load average: 0.16, 0.04, 0.01 9c40c7d871461fce4fbef21d33d0b350db3bc158

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -32,9 +32,12 @@ typedef struct {
   vaddr_t eip;
   struct Eflag{
     unsigned int CF: 1;
+    unsigned int   : 5;
     unsigned int ZF: 1;
     unsigned int SF: 1;
+    unsigned int   : 1;
     unsigned int IF: 1;
+    unsigned int   : 1;
     unsigned int OF: 1;
   } eflag;
 } CPU_state;
