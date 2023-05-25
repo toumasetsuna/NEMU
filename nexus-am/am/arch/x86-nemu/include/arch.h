ae88fd2187252fe6ddb3f1e7a3306652a6f17d0c
tracer-ics2017 <tracer@njuics.org>
2023-05-25 14:27:52 +0000

    >  run 2012109 ics Linux 444201d238c3 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 GNU/Linux  14:27:52 up 3 days, 19:39,  0 users,  load average: 0.24, 0.08, 0.02 1bcbf4e4b26965bec1bad4a12ce0dbc15b9d8ee

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -3,12 +3,24 @@
 
 #include <am.h>
 
+
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
-  int       irq;
+  uintptr_t edi;
+  uintptr_t esi;
+  uintptr_t ebp;
+  uintptr_t esp;
+  uintptr_t ebx;
+  uintptr_t edx;
+  uintptr_t ecx;
+  uintptr_t eax;
+  int irq;
+  uintptr_t error_code;
+  uintptr_t eip;
+  uintptr_t cs;
+  uintptr_t eflags;
 };
 
 #define SYSCALL_ARG1(r) 0
