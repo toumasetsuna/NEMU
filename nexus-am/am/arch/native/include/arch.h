88338f2753172c6f7f973d127c1b82562730f1df
tracer-ics2017 <tracer@njuics.org>
2023-05-26 18:17:52 +0000

    >  run 2012109 ics Linux 444201d238c3 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 GNU/Linux  18:17:52 up 10:34,  1 user,  load average: 0.00, 0.00, 0.03 5d9616c7448784ed9c0cb2359b8ee0267bd00c79

--- nexus-am/am/arch/native/include/arch.h
+++ nexus-am/am/arch/native/include/arch.h
@@ -3,8 +3,24 @@
 
 #include <unistd.h>
 #include <sys/types.h>
-
+#define SYSCALL_ARG1(r) r->eax
+#define SYSCALL_ARG2(r) r->ebx
+#define SYSCALL_ARG3(r) r->ecx
+#define SYSCALL_ARG4(r) r->edx
 struct _RegSet {
+ uintptr_t edi;
+  uintptr_t esi;
+  uintptr_t ebp;
+  uintptr_t esp;
+  uintptr_t ebx;
+  uintptr_t edx;
+  uintptr_t ecx;
+  uintptr_t eax;
+  int irq;
+  uintptr_t error_code;
+  uintptr_t eip;
+  uintptr_t cs;
+  uintptr_t eflags;
 };
 
 #endif
