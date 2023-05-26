ca5008306b36214f74aed5ca3b1ea744a2e69e53
tracer-ics2017 <tracer@njuics.org>
2023-05-26 15:30:06 +0000

    >  run 2012109 ics Linux 444201d238c3 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 GNU/Linux  15:30:06 up  7:46,  0 users,  load average: 0.21, 0.07, 0.02 ee40141d378076c636050a7ee648acd971c2be1

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,9 @@ extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
-      do_syscall(r);
       printf("System Call\n");
+      do_syscall(r);
+      
       break;
     default: panic("Unhandled event ID = %d", e.event);
   }
