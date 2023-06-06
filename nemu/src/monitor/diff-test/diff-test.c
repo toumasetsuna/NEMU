921ecf210d50091bff8138783c601992f4315631
tracer-ics2017 <tracer@njuics.org>
2023-06-06 02:25:21 -0700

    >  compile 2012109 setsuna Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:25:21 up  1:08,  1 user,  load average: 0.54, 0.30, 0.26 1d2e40d1a98687b9156462a9902684bb45da2c7a

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -214,7 +214,7 @@ void difftest_step(uint32_t eip) {
     //diff=true;
   }
  // printf("eip qemu: 0x%08x\n",r.eip);
- printf("eip: 0x%08x\n",cpu.eip);
+ //printf("eip: 0x%08x\n",cpu.eip);
   if (diff) {
     //
     printf("eip: 0x%08x\n",cpu.eip);
