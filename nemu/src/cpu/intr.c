0d478d2a97090b047d082bc8903b587cf5ff171e
tracer-ics2017 <tracer@njuics.org>
2023-06-12 02:28:40 -0700

    >  compile 2012109 setsuna Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:28:40 up 1 day, 17:44,  1 user,  load average: 0.28, 0.21, 0.14 e2dfbd68b363f0c6676ea03bf36844f2c6b63f12

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,6 +11,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  Log("%d",NO);
   rtl_push(&cpu.eflag);
   rtl_push(&cpu.CS);
   rtl_push(&ret_addr);
