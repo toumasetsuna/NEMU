30c9fc8dd8a973a1c8795da7100b51739b18d8b9
tracer-ics2017 <tracer@njuics.org>
2023-06-13 09:21:10 -0700

    >  run 2012109 setsuna Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:21:10 up 2 days, 13:21,  3 users,  load average: 0.08, 0.13, 0.12 8b3e366cfd0be238cb992d9839d903b8459f6a3c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,6 +34,7 @@ static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
   Log("dispinfo_read");
+  Log("%d",offset);
   memcpy(buf, dispinfo+offset, len);
 }
 void fb_write(const void *buf, off_t offset, size_t len) { 
