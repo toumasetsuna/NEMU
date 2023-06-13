7c26a04da61a1c3855abca65d37b52ef6fc954d2
tracer-ics2017 <tracer@njuics.org>
2023-06-13 03:07:31 -0700

    >  run 2012109 setsuna Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:07:31 up 2 days,  7:21,  2 users,  load average: 0.63, 0.51, 0.32 53a554ecdc03e6632445a93fbb0fa003536eb7

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,7 @@
 #include "proc.h"
 extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
+  static int num=0;
   switch (e.event) {
     case _EVENT_SYSCALL:
       //printf("System Call\n");
@@ -14,8 +15,11 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       break;
     default: panic("Unhandled event ID = %d", e.event);
   }
-  
-  return schedule(r);
+  if(num==100){
+    num=0;
+    return schedule(r);
+  }
+  return NULL;
 }
 
 void init_irq(void) {
